6bcbf8df5db6fa3e189ed91f7404125562b7dbd5
Cherlas <vm@li.cm>
2019-04-27 21:16:13 +0800

    add  NO2_AddTwoNumbers problem

--- cn/istarx/java/NO2_AddTwoNumbers.java
+++ cn/istarx/java/NO2_AddTwoNumbers.java
@@ -1,55 +1,63 @@
 /**
  * Definition for singly-linked list.
  */
-private class ListNode {
-	int val;
-	ListNode next;
-	ListNode(int x) { val = x; }
-	@Override
-	public String toString() {
-		if (next != null) {
-			return String.valueOf(val) + "->";
-		} else {
-			return String.valueOf(val);
-		}
-	}
- }
+package cn.istarx.java;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        if (next != null) {
+            return String.valueOf(val) + "->";
+        } else {
+            return String.valueOf(val);
+        }
+    }
+}
 
 class NO2_AddTwoNumbers {
-	public static void main(String[] args) {
-		ListNode res = addTwoNumbers(l1, l2);
-	}
-	
-	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		if (l1 == null || l2 == null) {
-			return l1 == null ? l2 == null ? null : l2 : l1;
-		}
-		int upNum = 0;
-		ListNode current = new ListNode(0);
-		ListNode res = current;
-		while (l1 != null || l2 != null) {
-			int tmp = 0;
-			if (l1 != null) {
-				tmp += l1.val;
-			}
-			if (l2 != null) {
-				tmp += l2.val;
-			}
-			tmp += upNum;
-			ListNode tmpNode= new ListNode(tmp % 10);
-			current.next = tmpNode;
-			current = tmpNode;
-			upNum = tmp / 10;
-			if (l1 != null) {
-				l1 = l1.next;
-			}
-			if (l2 != null) {
-				l2 = l2.next;
-			}
-		}
-		if (upNum != 0) {
-			current.next = new ListNode(upNum);
-		}
-		return res.next;
-	}
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode res = addTwoNumbers(l1, l2);
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 == null ? null : l2 : l1;
+        }
+        int upNum = 0;
+        ListNode current = new ListNode(0);
+        ListNode res = current;
+        while (l1 != null || l2 != null) {
+            int tmp = 0;
+            if (l1 != null) {
+                tmp += l1.val;
+            }
+            if (l2 != null) {
+                tmp += l2.val;
+            }
+            tmp += upNum;
+            ListNode tmpNode = new ListNode(tmp % 10);
+            current.next = tmpNode;
+            current = tmpNode;
+            upNum = tmp / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (upNum != 0) {
+            current.next = new ListNode(upNum);
+        }
+        return res.next;
+    }
 }
